f7bf7c5dcb60ac83e997ba141ce26909b56282ea
Farid-2000 <faridzeyn2000@gmail.com>
2024-06-23 13:03:39 +0400

    readme added

---
+++ README.md
@@ -0,0 +1,13 @@
+# COVID-19 Tracker
+
+![COVID-19 Tracker](https://i.ibb.co/X87BqVY/Screenshot-2020-04-13-at-10-14-58.png)
+
+## Introduction
+This is a code repository for the corresponding covid 19 tracker project. 
+
+We're going to use React, Charts.JS and Material UI.
+
+API used: https://covid19.mathdro.id/api
+
+Setup:
+- run ```npm i && npm start```
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { Line, Bar } from 'react-chartjs-2'
 
-import { fetchDailyData } from '../../api'
+import { fetchDailyData } from '../../api/index.js'
 
 import styles from './Chart.module.css'
 
-const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+
+const Chart = ({ data: { confirmed, recovered, deaths },country }) => {
 	const [dailyData, setDailyData] = useState({})
 
 	useEffect(() => {
